47f03cf47e08baf4a4215e98fd698ab91b924cbd
boppreh <boppreh@gmail.com>
2014-03-30 00:24:58 -0300

    Add more structure docs and comments

--- structures.py
+++ structures.py
@@ -22,13 +22,22 @@ class Identity(namedtuple('Identity', ['subject', 'domain', 'nonce', 'public_key
     Revocation Key Hash -> hash(hash(nonce + domain + master key))
     """
     def to_json(self):
+        """
+        Converts this identity to a JSON string.
+        """
         return json.dumps(self._asdict())
 
     def __repr__(self):
+        """
+        Returns the JSON representation of this identity.
+        """
         return self.to_json()
 
     @staticmethod
     def from_json(dump):
+        """
+        Creates an identity from a JSON string.
+        """
         return Identity(**json.loads(dump))
 
 class User(object):
@@ -40,50 +49,73 @@ class User(object):
     RSA_KEY_BIT_LENGTH = 2048
 
     def __init__(self, master_key=None):
+        """
+        Creates a new user. If not given a master key, a new random one
+        is generated.
+        """
         self.master_key = master_key or crypto_random(User.MASTER_KEY_BIT_LENGTH / 8)
         self.keys_by_domain = {}
 
     def load_identity(self, identity):
+        """
+        Re-generates the private key for the identity domain and caches
+        it in this User instance.
+        """
         nonce = from_base64(identity.nonce)
         domain = identity.domain
         self.keys_by_domain[domain] = self._generate_keypair(nonce, domain)
 
-    def get_subject(self, domain_unicode):
-        core = domain_unicode.encode('utf-8') + self.master_key
+    def get_subject(self, domain):
+        """
+        Returns the Base64 identifier of this user in the given domain.
+
+        subject = sha256(domain + master key)
+        """
+        core = domain.encode('utf-8') + self.master_key
         return base64(from_hex(sha256(core)))
 
-    def build_identity(self, domain_unicode):
+    def build_identity(self, domain):
+        """
+        Creates an Identity instance for this user in the given domain.
+        The corresponding private key is cached in this User instance.
+        """
         nonce = crypto_random(32)
 
-        keypair = self._generate_keypair(nonce, domain_unicode)
-        self.keys_by_domain[domain_unicode] = keypair
+        keypair = self._generate_keypair(nonce, domain)
+        # Cache key pair.
+        self.keys_by_domain[domain] = keypair
+        # PEM-encoded public key.
         public_key = keypair.publickey.serialize().decode('utf-8')
 
-        subject = self.get_subject(domain_unicode)
-        revocation_key_hash = sha256(self._get_revocation_key(domain_unicode, nonce))
+        subject = self.get_subject(domain)
+        revocation_key_hash = sha256(self._get_revocation_key(domain, nonce))
 
         return Identity(subject,
-                        domain_unicode,
+                        domain,
                         base64(nonce),
                         public_key,
                         revocation_key_hash,
+                        # TODO: signature
                         None)
 
-    def _generate_keypair(self, nonce, domain_unicode):
+    def _generate_keypair(self, nonce, domain):
         """
         Generates a deterministic RSA keypair from a random number generator
         seed in bytes.
         """
-        domain_bytes = domain_unicode.encode('utf-8')
+        domain_bytes = domain.encode('utf-8')
+        # TODO: check if this usage is correct. This is a fairly internal
+        # class of the PyCryto package (this is not the original class name,
+        # check the import).
         r = FortunaPrng()
         r.reseed(nonce + domain_bytes + self.master_key)
         return RsaKeypair(User.RSA_KEY_BIT_LENGTH, r.pseudo_random_data)
 
-    def _get_revocation_key(self, domain_unicode, nonce):
+    def _get_revocation_key(self, domain, nonce):
         """
         Generates a revocation key for a given domain.
         """
-        domain_bytes = domain_unicode.encode('utf-8')
+        domain_bytes = domain.encode('utf-8')
         return sha256(nonce + domain_bytes + self.master_key)
 
     def revoke(self, identity):
