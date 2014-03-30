f10eae7ed6a9530f4a975d125eb76d43c22c10b7
boppreh <boppreh@gmail.com>
2014-03-30 19:30:39 -0300

    Add (currently non-working) sample

---
+++ sample.py
@@ -0,0 +1,54 @@
+import server
+import client
+
+service = server.Service('example.org')
+user = client.User()
+
+# User backs up master key.
+master_key_backup = user.master_key
+
+# User registers with service.
+identity = user.build_identity('example.org')
+service.register_identity(identity)
+
+# User accesses service webpage. This page contains a login QR code.
+nonce = service.make_session_nonce()
+
+# User reads QR code with cellphone.
+# Signs the nonce...
+signed_nonce = user.sign(nonce, 'example.org')
+# And sends this info back to the service.
+service.session_login(nonce, signed_nonce, identity.subject)
+
+# Server now nows which user is behind that session. User is logged in.
+# Client and server can talk with TLS, using the server's cert and the user's
+# public key from the identity.
+
+# User logouts, releasing the session.
+# This must be done in an authenticated channel to avoid DOS.
+service.session_logout(nonce)
+
+# The user promptly loses his phone.
+user = None
+
+# Thankfully we have the backup.
+new_user = client.User(master_key_backup)
+
+# Problem: the identity created was not backed up.
+# Solution: ask the server.
+subject = new_user.get_subject('example.org')
+identity_nonce = service.fetch_identity_nonce(subject)
+
+# We could reload the identity and continue using the service as normal:
+# user.load_identity('example.org', nonce)
+# But whoever got the phone now has our keys, so we better revoke them.
+revocation_key = new_user.revoke('example.org', identity_nonce)
+new_identity = user.build_identity('example.org')
+# We did not change the master key, so the subject remains the same.
+service.revoke(subject, subject, revocation_key, new_identity)
+
+# We now have a brand new identity to use:
+nonce = service.make_session_nonce()
+signed_nonce = user.sign(nonce, 'example.org')
+service.session_login(nonce, signed_nonce, identity.subject)
+service.session_logout(nonce)
